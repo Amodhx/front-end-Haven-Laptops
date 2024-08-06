3c55dfd0c2a72eab0e9e1ec0cee7fdd72731e5e4
Amodhx <amxdhnanditha@gmail.com>
2024-08-06 14:52:58 +0530

    home best product done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeImageComponent } from './home-image/home-image.component';
+import { BestProductsComponent } from './best-products/best-products.component';
+import { BrowseCategoriesComponent } from './browse-categories/browse-categories.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeImageComponent],
+  imports: [HeaderComponent,HomeImageComponent,BestProductsComponent,BrowseCategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
