b9a75e7ebe1352eb21e3c4cc9423085a984b6ba7
Amodhx <amxdhnanditha@gmail.com>
2024-08-06 15:27:21 +0530

    home banner done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from '../header/header.component';
 import { HomeImageComponent } from './home-image/home-image.component';
 import { BestProductsComponent } from './best-products/best-products.component';
 import { BrowseCategoriesComponent } from './browse-categories/browse-categories.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeImageComponent,BestProductsComponent,BrowseCategoriesComponent],
+  imports: [HeaderComponent, HomeImageComponent, BestProductsComponent, BrowseCategoriesComponent, HomeBannerComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
